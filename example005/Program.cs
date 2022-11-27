595215c9f29a30d99a618c7e2a5cc63a3a27d08b
ShamaKung <shamaturin@gmail.com>
2022-11-27 23:20:29 +0500

    example004 random

---
+++ example005/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 10);  //1, 2, 3, 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int sum = numberA + numberB;
+Console.WriteLine(sum);
