3b25e33f3d5142e5a6fbde4d7feab0a120b4ee53
ShamaKung <shamaturin@gmail.com>
2022-11-27 23:28:34 +0500

    example006 hello Masha

---
+++ example006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+
+if(username.ToLower=="Маша")
+{
+    Console.WriteLine("Приветььььь");
+}
+else
+{
+    Console.Write("Дратути ");
+    Console.WriteLine(username);
+}
