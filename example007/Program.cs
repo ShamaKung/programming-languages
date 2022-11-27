834452f9e61ae25ef358637c64f31de69701466d
ShamaKung <shamaturin@gmail.com>
2022-11-27 23:34:51 +0500

    example007 Bases

---
+++ example007/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 5;
+int b = 10;
+int c = 8;
+int d = 1;
+int e = 3;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
